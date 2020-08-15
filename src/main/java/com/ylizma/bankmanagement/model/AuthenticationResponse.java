c168a67a4bcdfeaa43fad025a6b303951c061e69
ylizma <youssefamzil98@gmail.com>
2020-08-15 12:55:07 +0100

    adding JWT Authentication

---
+++ src/main/java/com/ylizma/bankmanagement/model/AuthenticationResponse.java
@@ -0,0 +1,12 @@
+package com.ylizma.bankmanagement.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthenticationResponse {
+    private String jwt;
+}
---
+++ src/main/java/com/ylizma/bankmanagement/service/UserDetailsService.java
@@ -0,0 +1,25 @@
+package com.ylizma.bankmanagement.service;
+
+import com.ylizma.bankmanagement.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+@Service
+public class UserDetailsService implements org.springframework.security.core.userdetails.UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        Optional<com.ylizma.bankmanagement.model.User> user = userRepository.findUserByUserName(userName);
+        return user.map(value -> new User(value.getUserName(), value.getPassword(), new ArrayList<>()))
+                .orElseThrow(() -> new UsernameNotFoundException(userName + " Not found !!!"));
+    }
+}
